0a0a7785a0d21b112357af47be3bf1588cdf185a
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-23 16:43:31 +0100

    added a randomiser to select which pokemon moves

--- js/main.js
+++ js/main.js
@@ -12,23 +12,26 @@ $(function () {
 	});
 
 
-
-	$charmander.animate({bottom:'150px'}, function () {
-	    setTimeout(function () {
-            $charmander.animate({bottom:'5px'});
-        	},500);
-		});
-	},2000);
+	pokemonMovement($pokemon);
 
 
 
 
 
+	function pokemonMovement ($pokemon) {
+		setInterval(function () {
+			$pokemon.eq(randomNumber(3)).animate({bottom:'150px'}, function () {
+	    		setTimeout(function () {
+            		$pokemon.animate({bottom:'5px'});
+        		},500);
+			});
+		},2000);
+	}
 
-
+	
 	function randomNumber (number) {
-		var min = Math.ceil(1);
-		var max = Math.floor(number+1);
+		var min = Math.ceil(0);
+		var max = Math.floor(number);
 		var num = Math.floor(Math.random() * (max - min)) + min;
 		return num;
 	} 
