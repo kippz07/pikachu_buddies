d3ad29cc70d48bc824043c666d9ad0733eebbb03
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-24 15:11:24 +0100

    added generation of new pokemon when one is clicked on

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
 $(function () {
 
-	$charmander = $('#charmander');
-	$squirtle = $('squirtle');
-	$bulbasaur = $('#bulbasaur');
-
+	$main = $('main');
 	$pokemon = $('.pokemon');
+	$score = $('#scoreNumber');
+
+	var playerScore = 0;
 
 	$pokemon.click(function (event) {
-		console.log('clicked');
+		if ($(this).is('#fire')) {
+			playerScore += 10;
+			$score.html(playerScore);
+		}
 		$(this).fadeToggle();
+		$(this).remove();
+		$main.append(generatePokemon(3, $(this).attr('class')));
 	});
 
 
 	pokemonMovement($pokemon);
 
 
-
+	function generatePokemon (num, pokeClass) {
+		var $newPokemon = $pokemon.eq(randomNumber(num));
+		return '<img id="' + $newPokemon.attr('id') + '" class="' + pokeClass + '" src="' + $newPokemon.attr('src') + '">';
+	}
 
 
 	function pokemonMovement ($pokemon) {
 		setInterval(function () {
-			$pokemon.eq(randomNumber(3)).animate({bottom:'150px'}, function () {
+			var $this = $pokemon.eq(randomNumber(3));
+			$this.animate({bottom:'150px'}, function () {
 	    		setTimeout(function () {
-            		$pokemon.animate({bottom:'5px'});
+            		$this.animate({bottom:'5px'});
         		},500);
 			});
 		},2000);
