eb7f3b9d76e212132c8a65468570f7919519fa63
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-24 16:34:35 +0100

    newly generated pokemon now have a random type

--- js/main.js
+++ js/main.js
@@ -6,37 +6,61 @@ $(function () {
 
 	var playerScore = 0;
 
-	$pokemon.click(function (event) {
-		if ($(this).is('#fire')) {
+	$main.on('click', '.pokemon', function (event) {
+		if ($(this).is('.fire')) {
 			playerScore += 10;
 			$score.html(playerScore);
 		}
 		$(this).fadeToggle();
 		$(this).remove();
-		$main.append(generatePokemon(3, $(this).attr('class')));
+		$main.append(generatePokemon(3, $(this).attr('id')));
 	});
 
 
-	pokemonMovement($pokemon);
+	pokemonMovement(3, $pokemon);
 
 
-	function generatePokemon (num, pokeClass) {
+	function generatePokemon (num, pokeId) {
 		var $newPokemon = $pokemon.eq(randomNumber(num));
-		return '<img id="' + $newPokemon.attr('id') + '" class="' + pokeClass + '" src="' + $newPokemon.attr('src') + '">';
+		return '<img id="' + pokeId + '" class="' + $newPokemon.attr('class') + '" src="' + $newPokemon.attr('src') + '">';
 	}
 
 
-	function pokemonMovement ($pokemon) {
+	function pokemonMovement (num, $pokemon) {
+
+		var $this = 0;
+		//var $newPokemon = 0;
+
 		setInterval(function () {
-			var $this = $pokemon.eq(randomNumber(3));
+			 $this = $pokemon.eq(randomNumber(num));
+			 //$newPokemon = $pokemon.eq(randomNumber(num));
+			//console.log($newPokemon);
 			$this.animate({bottom:'150px'}, function () {
-	    		setTimeout(function () {
-            		$this.animate({bottom:'5px'});
-        		},500);
+		   		setTimeout(function () {
+	           		$this.animate({bottom:'5px'});
+	       		},500);
+		   		setTimeout(function () {
+		   			$this.removeClass($this.attr('class'));
+		   			$this.addClass(randomClass(randomNumber(3)));
+		   		},800);
 			});
+			
+			
 		},2000);
+
 	}
 
+
+	function randomClass (number) {
+		switch (number) {
+			case 0: return 'pokemon fire';
+				break;
+			case 1: return 'pokemon water';
+				break;
+			case 2: return 'pokemon grass';
+				break;
+		}
+	}
 	
 	function randomNumber (number) {
 		var min = Math.ceil(0);
