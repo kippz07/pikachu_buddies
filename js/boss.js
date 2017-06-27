d3f2a50e7d50c832184cdb508e6d22341eb80dfd
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-27 14:33:56 +0100

    added boss attacks

--- js/boss.js
+++ js/boss.js
@@ -3,7 +3,12 @@ $(function() {
 	var bossHealth = 500;
 	var remHealth = 100;
 	var $bossHealthbar = $('.healthbar');
+	var pikachuHealth = 500;
+	var pikaremHealth = 100;
+	var $pikachuHealthbar = $('.healthbar');
+	var $hideAttacks = $('#hideAttacks');
 	var pp = 0;
+	var count = 0;
 
 
 	var thunderbolt = {"dmg": 95, "acc": 100, "pp": 15, "flinch": 10};
@@ -32,35 +37,103 @@ $(function() {
 
 	$att1.click(function (event) {
 		pp = thunderbolt.pp;
-		remHealth = remHealth - (thunderbolt.dmg * 100 / bossHealth);
-		$bossHealthbar.css('width', remHealth + '%');
-		pp--;
-		$pp1.html(pp);
+		if (pp > 0) {
+			remHealth = remHealth - (thunderbolt.dmg * 100 / bossHealth);
+			$bossHealthbar.css('width', remHealth + '%');
+			pp--;
+			$pp1.html(pp);
+		}
+		count++;
+		play();
 	})
 
 	$att2.click(function (event) {
 		pp = hiddenPower.pp;
-		remHealth = remHealth - (hiddenPower.dmg * 100 / bossHealth);
-		$bossHealthbar.css('width', remHealth + '%');
-		pp--;
-		$pp2.html(pp);
+		if (pp > 0) {
+			remHealth = remHealth - (hiddenPower.dmg * 100 / bossHealth);
+			$bossHealthbar.css('width', remHealth + '%');
+			pp--;
+			$pp2.html(pp);
+		}
+		count++;
+		play();
 	})
 
 	$att3.click(function (event) {
 		pp = thunder.pp;
-		remHealth = remHealth - (thunder.dmg * 100 / bossHealth);
-		$bossHealthbar.css('width', remHealth + '%');
-		pp--;
-		$pp3.html(pp);
+		if (pp > 0){
+			remHealth = remHealth - (thunder.dmg * 100 / bossHealth);
+			$bossHealthbar.css('width', remHealth + '%');
+			pp--;
+			$pp3.html(pp);
+		}
+		count++;
+		play();	
 	})
 
 	$att4.click(function (event) {
 		pp = thunderShock.pp;
-		remHealth = remHealth - (thunderShock.dmg * 100 / bossHealth);
-		$bossHealthbar.css('width', remHealth + '%');
-		pp--;
-		$pp4.html(pp);
+		if (pp > 0) {
+			remHealth = remHealth - (thunderShock.dmg * 100 / bossHealth);
+			$bossHealthbar.css('width', remHealth + '%');
+			pp--;
+			$pp4.html(pp);
+		}
+		count++;
+		play();
 	})
+
+	function play () {
+		if (count % 2 === 0) {
+			$hideAttacks.hide();
+
+		} else {
+			$hideAttacks.show();
+			blastoiseAttack();
+			count++;
+		}
+	}
+
+	function blastoiseAttack () {
+		var number = randomNumber(4);
+		switch (number) {
+			case 0:
+				pp = hydroPump.pp;
+				if (pp > 0) {
+					pikaremHealth = pikaremHealth - (hydroPump.dmg * 100 / pikachuHealth);
+					$pickachuHealthbar.css('width', pikaremHealth + '%');
+					pp--;
+					break;
+				}
+				
+			case 1:
+				pp = darkPulse.pp;
+				if (pp > 0) {
+					pikaremHealth = pikaremHealth - (darkPulse.dmg * 100 / pikachuHealth);
+					$pickachuHealthbar.css('width', pikaremHealth + '%');
+					pp--;
+					break;
+				}
+				
+			case 2:
+				pp = iceBeam.pp;
+				if (pp > 0) {
+					pikaremHealth = pikaremHealth - (iceBeam.dmg * 100 / pikachuHealth);
+					$pickachuHealthbar.css('width', pikaremHealth + '%');
+					pp--;
+					break;
+				}
+				
+			case 3:
+				pp = dragonPulse.pp;
+				if (pp > 0) {
+					pikaremHealth = pikaremHealth - (dragonPulse.dmg * 100 / pikachuHealth);
+					$pickachuHealthbar.css('width', pikaremHealth + '%');
+					pp--;
+					break;
+				}			
+		}
+	}
 
 	function randomNumber (number) {
 		var min = Math.ceil(0);
