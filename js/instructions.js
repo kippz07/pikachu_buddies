27207299bf088f7bc18041b7715a067fd18d1c73
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-28 14:13:07 +0100

    added instructions for main game

---
+++ js/instructions.js
@@ -0,0 +1,10 @@
+$(function () {
+
+	Pikachu is lonely and wants some friends but is very particular about which pokemon it wants to play with. Gain points by clicking on the pokemon type indicated in the box at the top of the screen. Beware, this type could change at any time.... clickin on the wrong pokemon will lose you a life.
+
+		There are three types of pokemon in this game: fire, water and grass. Each type is associated with a pokemon.
+
+
+
+
+});
